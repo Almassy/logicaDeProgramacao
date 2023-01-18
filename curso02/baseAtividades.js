f37179a9edc7c8a483d0f1724847c1e837acea0d
Almassy <119798414+Almassy@users.noreply.github.com>
2023-01-18 13:31:01 -0300

    montando a base das atividades

---
+++ curso02/baseAtividades.js
@@ -0,0 +1,17 @@
+function makeRectangle(x,y,width,height,color){
+    var canvas = document.querySelector("canvas");
+    var pencil = canvas.getContext("2d");
+
+    pencil.fillStyle = color;
+    pencil.fillRect (x,y,width,height);
+    pencil.strokeStyle = "black";
+    pencil.strokeRect (x,y,width, height);
+} 
+function makeText (x,y,text){
+    var canvas = document.querySelector("canvas");
+    var pencil = canvas.getContext("2d");
+
+    pencil.font = "15px Georgia";
+    pencil.fillStyle = "black";
+    pencil.fillText(text,x,y);
+}
